d29cf8986b7fdce6ef42c3a8795ceb81129bd370
Deusdenith Marquez <38871703+djmu677@users.noreply.github.com>
2026-09-13 23:01:20 -0300

    feat: separar biblioteca multimedia

---
+++ src/app/(app)/media/page.tsx
@@ -0,0 +1,7 @@
+import { MediaLibraryClient } from "@/components/media/media-library-client";
+
+export const dynamic = "force-dynamic";
+
+export default function MediaPage() {
+  return <MediaLibraryClient />;
+}
